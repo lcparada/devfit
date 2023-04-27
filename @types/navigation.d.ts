c28b675b3a671a358843b7f789b70a6b35ebc098
lcparada <125614063+lcparada@users.noreply.github.com>
2023-04-27 09:53:35 -0300

    chore: types of navigation

---
+++ @types/navigation.d.ts
@@ -0,0 +1,10 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Welcome: undefined;
+      Name: undefined;
+      DaysTraining: undefined;
+      Level: undefined;
+    }
+  }
+}
