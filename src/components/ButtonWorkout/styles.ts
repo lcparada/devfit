bf72eaffae42abddad45c710f86be3d9dad7cc8a
lcparada <loparada@gmail.com>
2023-05-01 14:26:00 -0300

    feat: create bottonWorkout

---
+++ src/components/ButtonWorkout/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const ButtonBottom = styled.View`
+  width: 100px;
+  height: 100px;
+  border-radius: 60px;
+  background-color: #f2f2f2;
+  margin-bottom: 40px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 90px;
+  height: 90px;
+  border-radius: 50px;
+  background-color: #d80042;
+  align-items: center;
+  justify-content: center;
+`;
