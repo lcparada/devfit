0f75c3827e5f6445ed6e22adad7a3f2f040dc0a9
lcparada <loparada@gmail.com>
2023-05-01 14:28:09 -0300

    feat: create home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+import Captions from "../../components/Captions";
+
+import { Body, Container, Header, TextCaption } from "./styles";
+
+import moment from "moment";
+
+import ListMonths from "../../components/ListMonths";
+
+import ListDays from "../../components/ListDays";
+import BalloonStatus from "../../components/BalloonStatus";
+
+export default function Home() {
+  const date = moment().subtract(3, "h");
+
+  const [selectedMonth, setSelectedMonth] = useState<number>(moment().month());
+  const [selectedDay, setSelectedDay] = useState<number>(
+    Number(date.format("D")) - 1
+  );
+
+  return (
+    <Container>
+      <Header>
+        <ListMonths
+          selectedMonth={selectedMonth}
+          setSelectedMonth={setSelectedMonth}
+        />
+        <ListDays
+          selectedMonth={selectedMonth}
+          setSelectedMonth={setSelectedMonth}
+          selectedDay={selectedDay}
+          setSelectedDay={setSelectedDay}
+        />
+      </Header>
+      <BalloonStatus selectedDay={selectedDay} selectedMonth={selectedMonth} />
+
+      <Body>
+        <TextCaption>Legenda:</TextCaption>
+        <Captions captionName="Hoje" captionColor="#2CBBBD" />
+        <Captions captionName="Treino feito" captionColor="#45CB85" />
+        <Captions captionName="Treino perdido" captionColor="#DB2B39" />
+        <Captions captionName="Dia de descanso" captionColor="#DADADA" />
+        <Captions captionName="Dia futuro" captionColor="#A9A7A7" />
+      </Body>
+    </Container>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+`;
+
+export const Body = styled.View`
+  margin-top: 10px;
+`;
+
+export const TextCaption = styled.Text`
+  font-family: Lexend_400Regular;
+  font-size: 12px;
+  margin-left: 10px;
+  margin-bottom: 5px;
+`;
