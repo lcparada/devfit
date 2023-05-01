0d82786e100d0ce188365085205de5d4746d912d
lcparada <loparada@gmail.com>
2023-05-01 14:26:36 -0300

    feat: create component ListDays

--- src/components/ListDays/index.tsx
+++ src/components/ListDays/index.tsx
@@ -1,29 +1,120 @@
-import { Days, TextDays } from "./styles";
+import {
+  DaysButton,
+  DaysItem,
+  DaysScroll,
+  DaysText,
+  Container,
+} from "./styles";
 
-import { TouchableOpacity } from "react-native";
+import { useState, useEffect, useRef, useContext } from "react";
 
-import { useState, useContext } from "react";
+import { calculateDaysInMonth } from "../../helpers/helpers";
+
+import { Dimensions, ScrollView } from "react-native";
+
+import moment from "moment";
 
 import { ProfileContext } from "../../contexts/informationProfile";
 
-type IProps = {
-  dayOfWeek: string;
+type ListDaysProps = {
+  selectedMonth: number;
+  setSelectedMonth: (number: number) => void;
+  selectedDay: number;
+  setSelectedDay: (number: number) => void;
 };
 
-export function ListDays(props: IProps) {
-  const { saveDaysTraining } = useContext(ProfileContext);
+export default function ListDays(props: ListDaysProps) {
+  const today = moment().subtract(3, "h");
 
-  const [color, setColor] = useState<string>("#d80042");
+  const dayRef = useRef<ScrollView>(null);
 
-  function sendData(day: string) {
-    setColor(saveDaysTraining(day));
+  const [days, setDays] = useState<number[]>(
+    calculateDaysInMonth(props.selectedMonth)
+  );
+  const [ninthScreen, setNinthScreen] = useState<number>(
+    Math.round(Dimensions.get("window").width / 9)
+  );
+  const [offSetW, setOffSetW] = useState<number>(
+    Math.round(Math.round(Dimensions.get("window").width - ninthScreen) / 2)
+  );
+
+  const { daysTraining } = useContext(ProfileContext);
+
+  function scrollToDay(selectedDay: number) {
+    dayRef.current?.scrollTo({
+      y: 0,
+      x: selectedDay * ninthScreen,
+      animated: true,
+    });
   }
 
+  useEffect(() => {
+    setDays(calculateDaysInMonth(props.selectedMonth));
+  }, [props.selectedMonth]);
+
+  useEffect(() => {
+    if (props.selectedMonth === moment().month()) {
+      props.setSelectedDay(Number(today.format("D")) - 1);
+      scrollToDay(props.selectedDay);
+    } else {
+      props.setSelectedDay(0);
+      scrollToDay(0);
+    }
+  }, [props.selectedMonth]);
+
+  useEffect(() => {
+    scrollToDay(props.selectedDay);
+  }, [props.selectedDay]);
+
+  useEffect(() => {
+    console.log(
+      moment(`2023-${props.selectedMonth + 1}-${props.selectedDay + 2}`).format(
+        "DD/MM/YYYY"
+      )
+    );
+    console.log(today.format("DD/MM/YYYY"));
+  }, []);
+
   return (
-    <TouchableOpacity onPress={() => sendData(props.dayOfWeek)}>
-      <Days backgroundColor={color}>
-        <TextDays>{props.dayOfWeek}</TextDays>
-      </Days>
-    </TouchableOpacity>
+    <Container>
+      <DaysScroll
+        ref={dayRef}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        decelerationRate={"fast"}
+        snapToInterval={ninthScreen}
+        contentContainerStyle={{
+          paddingRight: offSetW,
+          paddingLeft: offSetW,
+        }}
+      >
+        {days.map((day, key) => (
+          <DaysButton
+            key={key}
+            width={ninthScreen}
+            onPress={() => props.setSelectedDay(key)}
+            underlayColor={"transparent"}
+          >
+            <DaysItem
+              backgroundColor={
+                moment(`2023-${props.selectedMonth + 1}-${day + 1}`).format(
+                  "DD/MM/YYYY"
+                ) === today.format("DD/MM/YYYY")
+                  ? "#2CBBBD"
+                  : daysTraining.includes(
+                      moment(
+                        `2023-${props.selectedMonth + 1}-${day + 1}`
+                      ).format("dddd")
+                    )
+                  ? "#DB2B39"
+                  : "#DADADA"
+              }
+            >
+              <DaysText>{day}</DaysText>
+            </DaysItem>
+          </DaysButton>
+        ))}
+      </DaysScroll>
+    </Container>
   );
 }
--- src/components/ListDays/styles.ts
+++ src/components/ListDays/styles.ts
@@ -1,22 +1,39 @@
 import styled from "styled-components/native";
 
-type Props = {
+type DaysButtonProps = {
+  width: number;
+};
+
+type DaysItemProps = {
   backgroundColor: string;
 };
 
-export const Days = styled.View<Props>`
-  width: 120px;
+export const Container = styled.View`
+  width: 100%;
   height: 40px;
-  background-color: ${(props) => props.backgroundColor};
-  border-radius: 20px;
-  align-items: center;
-  justify-content: center;
-  margin-left: 12px;
-  margin-top: 15px;
+  row-gap: 100px;
 `;
 
-export const TextDays = styled.Text`
+export const DaysScroll = styled.ScrollView`
+  width: 100%;
+  height: 30px;
+`;
+
+export const DaysButton = styled.TouchableHighlight<DaysButtonProps>`
+  width: ${(props) => props.width}px;
+  height: 60px;
+`;
+
+export const DaysItem = styled.View<DaysItemProps>`
+  width: 70%;
+  height: 30px;
+  background-color: ${(props) => props.backgroundColor};
+  border-radius: 15px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DaysText = styled.Text`
   font-family: Lexend_400Regular;
-  font-size: 14px;
   color: white;
 `;
