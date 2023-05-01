ffdb6fe4bf5b5370ef376fb0241936fa417ba010
lcparada <loparada@gmail.com>
2023-05-01 14:28:46 -0300

    feat: create myworkouts screen

---
+++ src/screens/MyWorkouts/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Header } from "./styles";
+
+export default function MyWorkouts() {
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  );
+}
---
+++ src/screens/MyWorkouts/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  margin-top: 100px;
+`;
